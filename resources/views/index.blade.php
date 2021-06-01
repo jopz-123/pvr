77dd78d7d0323c4b32f8f9f13a226e749c7d4d12
jopz-123 <josephfrancizz009@gmail.com>
2021-06-01 12:29:01 +0530

    message

---
+++ resources/views/index.blade.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body style="background-color: rgb(7, 7, 7);">
+
+        
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h5 class="text-white h4">TODAYS BLOCKBUSTER MOVIES</h5>
+      <span class="text-muted">JAWS <br>
+                                JOKER <br>
+                                THE PRIEST<br>
+                                GODZILLA <br>
+                                NO TIME TO DIE </span>
+    </div>
+  </div>
+  
+  <nav class="navbar-dark navbar-dark bg-dark">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+
+  
+
+  <nav class="navbar navbar-light" style="background-color: #fceb00;" style="color: orange;">
+    <!-- Navbar content -->
+    <div class="container-fluid" style="background-color: #fce300f3;" style="color: orange;">
+      <a class="navbar-brand" href="#"><marquee><h1> PVR</h1></marquee></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        
+      </button>
+
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"><b>Search</b></button>
+            </form>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href=" /"><b>Home</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.pvrcinemas.com/about"><b>About Us</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login"><b>Login</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href=" /signin"><b>Sign In</b></a>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.pvrcinemas.com/cinemas/Kochi"><b>Ticket Bookings</b></a>
+          </li>
+
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cinemas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="https://www.pvrcinemas.com/offers"></a>Offers</li>
+            <li><a class="dropdown-item" href="https://www.pvrcinemas.com/giftcards"><b>Gift cards</b></a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#"></a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="https://www.pvrcinemas.com/pvr/imax" tabindex="-1" aria-disabled="true"><b>IMAX</b></a>
+        </li>
+      </ul>
+      <ul class="list-unstyled" style="font-size: 20px;color: rgb(10, 10, 10);">
+        <li><b>Phone: 91 - 484 - 2660601</b></li>
+        <li><b>Fax: 91 - 484 - 2426578</b></li>
+    </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <span class="navbar-text">
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-5 col-lg-5 col-md-5 col-xl-7 col-xxl-7">
+                  <img height="10px" width="250px" src="https://i1.wp.com/zeevector.com/wp-content/uploads/LOGO/PVR-Cinemas-Logo-PNG.png?resize=768%2C280&ssl=1" class="img-thumbnail" alt="...">
+              </div>
+              <div class="col col-12 col-sm-7 col-lg-7 col-md-7 col-xl-12 col-xxl-12">
+              </div>
+          </div>
+      </div>
+    </span>
+  </div>
+</nav>
+
+<centre>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <img height="500px" width="1300px" src="https://lh4.googleusercontent.com/proxy/hFy5jvhFScQoYRBr0sOv8GspzPj4iBkr_RPz8KSHR7Hb6EOEoOQ3zHlY0cX5uqgshkBLpw785G3jjDd78obFUhA2ZmeiI-OURJK4ZdJ16IlU_8KHIzo6eQWOvtjr6Wa_9CQywLkj7jLdYA=s0-d" class=" w-20lockpx0d-b" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img height="1000px" width="5px" src="https://bingeddata.s3.amazonaws.com/uploads/2021/01/The-Priest-_-Mammootty.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img height="500px" width="1300px" src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/03/Venom-Let-There-Be-Carnage.jpg?q=50&fit=contain&w=750&h=375&dpr=1.5" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img height="700px" width="1500px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a58a7719-0dcf-4e0b-b7bb-d2b725dbbb8e/ddy7ptk-e2cbec0e-45df-4709-8f06-e5d3eca07b1c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTU4YTc3MTktMGRjZi00ZTBiLWI3YmItZDJiNzI1ZGJiYjhlXC9kZHk3cHRrLWUyY2JlYzBlLTQ1ZGYtNDcwOS04ZjA2LWU1ZDNlY2EwN2IxYy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.AmhLzwm8VWPjb10y5fUzLyf4TIpMDcrtXQ9MVHqqdbw" class="d-block w-100" alt="...">
+    </div>
+  </div>
+
+  <div class="carousel-item">
+    <img height="1000px" width="5px" src="" class="d-block w-100" alt="...">
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</centre>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/signin.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+
+<body>
+  
+  
+
+<nav class="navbar navbar-light" style="background-color: #fceb00;" style="color: orange;">
+    <!-- Navbar content -->
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><marquee><h1> PVR</h1></marquee></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"><b>Search</b></button>
+            </form>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/"><b>Home</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.pvrcinemas.com/about"><b>About Us</b></a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="/login"><b>Login</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href=" /signin"><b>Sign In</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.pvrcinemas.com/cinemas/Kochi"><b>Ticket Bookings</b></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="https://www.pvrcinemas.com/cinemas/Kochi" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cinemas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="https://www.pvrcinemas.com/offers"></a>Offers</li>
+            <li><a class="dropdown-item" href="https://www.pvrcinemas.com/giftcards"><b>Gift cards</b></a></li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="https://www.pvrcinemas.com/pvr/imax" tabindex="-1" aria-disabled="true"><b>IMAX</b></a>
+            </li>
+          </ul>
+      <ul class="list-unstyled" style="font-size: 20px;color: rgb(10, 10, 10);">
+        <li><b>Phone: 91 - 484 - 2660601</b></li>
+        <li><b>Fax: 91 - 484 - 2426578</b></li>
+    </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <span class="navbar-text">
+      <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-5 col-lg-5 col-md-5 col-xl-7 col-xxl-7">
+                  <img height="10px" width="250px" src="https://i1.wp.com/zeevector.com/wp-content/uploads/LOGO/PVR-Cinemas-Logo-PNG.png?resize=768%2C280&ssl=1" class="img-thumbnail" alt="...">
+              </div>
+              <div class="col col-12 col-sm-7 col-lg-7 col-md-7 col-xl-12 col-xxl-12">
+              </div>
+          </div>
+      </div>
+    </span>
+  </div>
+</nav>
+
+<div class="container">
+  <div class="raw">
+    <div class="col ">
+          <table class="table table-borderless">
+            <tr>
+              <td>Email Address</td>
+              <td><input placeholder="Email@example.com" type="text" class="form-control"></td>
+            </tr>
+            <tr>
+              <td>Password</td>
+              <td><input placeholder="Password@gmail.com" type="text" class="form-control"></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><button class="btn btn-primary">Sign in</button></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><button class="btn btn-danger">Forgot Password</button></td>
+            </tr>
+          </table>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
